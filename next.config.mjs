0fd54e5eb4283a83bb82941cf01093d3a181d5ea
Arthur Angelici <arthurangelici@gmail.com>
2025-06-29 01:08:37 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  "env": {
+    "NEXTAUTH_URL": process.env.NEXTAUTH_URL,
+    "NEXTAUTH_SECRET": process.env.NEXTAUTH_SECRET,
+    "DATABASE_URL": process.env.DATABASE_URL
+  },
+  "build": {
+    "env": {
+      "NEXTAUTH_URL": process.env.NEXTAUTH_URL,
+      "NEXTAUTH_SECRET": process.env.NEXTAUTH_SECRET,
+      "DATABASE_URL": process.env.DATABASE_URL
+    }
+  }
+};
 
 export default nextConfig;
